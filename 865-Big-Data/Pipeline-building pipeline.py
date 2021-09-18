c22132b41955782777cbe77c3177de0b19170939
yujun.mu@queensu.ca <yujun.mu@queensu.ca>
2021-09-18 12:57:48 +0000

    recover

--- 865-Big-Data/Pipeline-building pipeline.py
+++ 865-Big-Data/Pipeline-building pipeline.py
@@ -93,7 +93,7 @@ eda.show()
 # pick and choose what pipeline you want.
 pipeline_test = [tokenizer, stopwordsRemover, counter]
 
-jxhdkajhdakjh
+
 eda = Pipeline(stages=pipeline_test).fit(df).transform(df)
 eda.show()
 
