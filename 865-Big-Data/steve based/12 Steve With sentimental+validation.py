4b0a5cf4b8e4130fbe8244c0f036ffcc9b91f7f3
yujun.mu@queensu.ca <yujun.mu@queensu.ca>
2021-09-28 23:58:09 +0000

    undersampling

--- 865-Big-Data/steve based/12 Steve With sentimental+validation.py
+++ 865-Big-Data/steve based/12 Steve With sentimental+validation.py
@@ -1,19 +1,12 @@
 # Databricks notebook source
-# DBTITLE 1,Make Sure to Update
 """
-kaggle: 
-trainingAUC xxxx
-xxx
-
-
-
 TODO:
-1. [DONE]proper missing value handle
-2. [MB]imbalance data
-3. [MB] topic
-4. [Done] sentiment analytis
+1. proper missing value handle
+2. imbalance data
+3. topic
+4. sentiment analytis
 5. [RIGHT-NOW] note down all the auc after adding new features
-6. [LAST] change the count vector values
+6. change the count vector values
 7. [RIGHT-NOW] count vector value add idf and summaryintion of features
 8. [Done]"verified", "overall",
 
@@ -27,46 +20,25 @@ For Imbalance
 +-----+-------+
 """
 
+
 # COMMAND ----------
 
-# DBTITLE 1,[Don't Touch]Load Data
-# from pyspark.sql.functions import to_date, year, month, dayofmonth, dayofyear, weekofyear
-# from pyspark.sql.functions import col, lit, when, split, size
-# from pyspark.ml import Pipeline
-# from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer, IDF, VectorAssembler, SQLTransformer
-# from sparknlp.annotator import ContextSpellCheckerModel, LemmatizerModel
-# from pyspark.ml.classification import LogisticRegression, GBTClassifier
-# from sparknlp.base import DocumentAssembler, Finisher
-# from sparknlp.annotator import Tokenizer, Normalizer, StopWordsCleaner, LemmatizerModel, DocumentNormalizer, UniversalSentenceEncoder, MultiClassifierDLModel
-# from pyspark.ml.clustering import LDA
-# from pyspark.sql.functions import to_date, year, month, dayofmonth, dayofyear, weekofyear
-# from pyspark.sql.functions import col, lit, when, split, size
-
+# DBTITLE 1,Load Data
 # Load in one of the tables
 df1 = spark.sql("select * from default.video_games_5")
 df2 = spark.sql("select * from default.books_5_small")
 df3 = spark.sql("select * from default.home_and_kitchen_5_small")
 df = df1.union(df2).union(df3)
-# print((df.count(), len(df.columns)))
-
-def resample(base_features, ratio, class_field, base_class):
-    pos = base_features.filter(col(class_field)==base_class)
-    neg = base_features.filter(col(class_field)!=base_class)
-    total_pos = pos.count()
-    total_neg = neg.count()
-    fraction=float(total_pos*ratio)/float(total_neg)
-    sampled = neg.sample(False,fraction)
-    return sampled.union(pos)
-
-# Resample
-df = resample(df,0.5,'label',1)
 print((df.count(), len(df.columns)))
 
 # COMMAND ----------
 
-# DBTITLE 1,[Don't touch]Transform Data
+# DBTITLE 1,Data Wrangling/Prep
 from pyspark.sql.functions import to_date, year, month, dayofmonth, dayofyear, weekofyear
 from pyspark.sql.functions import col, lit, when, split, size
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
 
 def featureEngineering(df): 
   res = df.withColumn(
@@ -75,11 +47,11 @@ def featureEngineering(df):
   )
 
   # Dates
-  res = res.withColumn('reviewTime_year', year(col('reviewTime')))
-  res = res.withColumn('reviewTime_month', month(col('reviewTime')))
-  res = res.withColumn('reviewTime_day', dayofmonth(col('reviewTime')))
-  res = res.withColumn('reviewTime_dayofy', dayofyear(col('reviewTime')))
-  res = res.withColumn('reviewTime_week_no', weekofyear(col('reviewTime')))
+#   res = res.withColumn('reviewTime_year', year(col('reviewTime')))
+#   res = res.withColumn('reviewTime_month', month(col('reviewTime')))
+#   res = res.withColumn('reviewTime_day', dayofmonth(col('reviewTime')))
+#   res = res.withColumn('reviewTime_dayofy', dayofyear(col('reviewTime')))
+#   res = res.withColumn('reviewTime_week_no', weekofyear(col('reviewTime')))
 
 #   #Reviewer Name
   res = res.withColumn('reviewerName_Shorthand', when(col('reviewerName').rlike('\\. '),True).otherwise(False))
@@ -103,24 +75,24 @@ def featureEngineering(df):
   res = res.withColumn('summaryNumberPeriod', size(split(col('summary'), r"\.")) - 1)
   
   return (res, [
-    "reviewTime_year",
-    "reviewTime_month",
-    "reviewTime_day",
-    "reviewTime_dayofy",
-    "reviewTime_week_no",
-    "reviewerName_Shorthand",
-    "reviewerName_isAmazon",
-    "reviewerName_capsName",
+#     "reviewTime_year",
+#     "reviewTime_month",
+#     "reviewTime_day",
+#     "reviewTime_dayofy",
+#     "reviewTime_week_no",
+#     "reviewerName_Shorthand",
+#     "reviewerName_isAmazon",
+#     "reviewerName_capsName",
     "reviewTextHasCapsWord",
-    "summaryHasCapsWord",
-    "reviewTextHasSwearWord",
-    "summaryHasSwearWord",
-    "reviewTextNumberExclamation",
-    "summaryNumberExclamation",
-    "reviewTextNumberComma",
-    "summaryNumberComma",
-    "reviewTextNumberPeriod",
-    "summaryNumberPeriod",
+#     "summaryHasCapsWord",
+#     "reviewTextHasSwearWord",
+#     "summaryHasSwearWord",
+#     "reviewTextNumberExclamation",
+#     "summaryNumberExclamation",
+#     "reviewTextNumberComma",
+#     "summaryNumberComma",
+#     "reviewTextNumberPeriod",
+#     "summaryNumberPeriod",
     
     "overall", 
     "verified"
@@ -128,31 +100,31 @@ def featureEngineering(df):
 
 df, featureList = featureEngineering(df)
 # For our intitial modeling efforts, we are not going to use the following features
-drop_list = ['unixReviewTime', 'image', 'style']
+drop_list = ['asin', 'reviewID', 'unixReviewTime','reviewTime', 'image', 'style', 'reviewerName']
 df = df.select([column for column in df.columns if column not in drop_list])
 df = df.na.drop(subset=["reviewText", "label", "summary"])
 print((df.count(), len(df.columns)))
 
 # class Weight - garbage
-# import pandas as pd
-# counts = df.groupBy('label').count().toPandas()
-# # Counts
-# count_fraud = counts[counts['label']==1]['count'].values[0]
-# count_total = counts['count'].sum()
-# # Weights
-# c = 2
-# weight_fraud = count_total / (c * count_fraud)
-# weight_no_fraud = count_total / (c * (count_total - count_fraud))
-# df = df.withColumn("classWeightCol", when(col("label") ==1, weight_fraud).otherwise(weight_no_fraud))
+import pandas as pd
+counts = df.groupBy('label').count().toPandas()
+# Counts
+count_fraud = counts[counts['label']==1]['count'].values[0]
+count_total = counts['count'].sum()
+# Weights
+c = 2
+weight_fraud = count_total / (c * count_fraud)
+weight_no_fraud = count_total / (c * (count_total - count_fraud))
+df = df.withColumn("classWeightCol", when(col("label") ==1, weight_fraud).otherwise(weight_no_fraud))
+
 
 
 # Split
 (trainingData, testingData) = df.randomSplit([0.8, 0.2], seed = 47)
-trainingData.columns
 
 # COMMAND ----------
 
-# DBTITLE 1,[Don't Touch] NLP tuning
+# DBTITLE 1,Create a Data Transformation/Preprocessing Pipeline
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer, IDF, VectorAssembler, SQLTransformer
 from sparknlp.annotator import ContextSpellCheckerModel, LemmatizerModel
@@ -223,28 +195,32 @@ def NLPPipe(fieldname):
 
 
   # IDF
-  idf = IDF(inputCol=f"{fieldname}_rawFeatures", outputCol=f"{fieldname}_idfFeatures", minDocFreq=5)
+#   idf = IDF(inputCol=f"rawFeatures", outputCol=f"idfFeatures", minDocFreq=5)
 
+
+#   cleaned_token_size = SQLTransformer(
+#       statement = f"SELECT * , size({fieldname}_filtered) AS {fieldname}_tokenSize FROM __THIS__"
+#   )
   cleaned_token_size = SQLTransformer(
       statement = f"SELECT * , size({fieldname}_token_features) AS {fieldname}_tokenSize FROM __THIS__"
   )
   
   # sentimental
-#   useEmbeddings = UniversalSentenceEncoder.pretrained() \
-#     .setInputCols(f"{fieldname}_document") \
-#     .setOutputCol(f"{fieldname}_sentence_embeddings")
-#   multiClassifierDl = MultiClassifierDLModel.pretrained() \
-#       .setInputCols(f"{fieldname}_sentence_embeddings") \
-#       .setOutputCol(f"{fieldname}_sentimental_classifications")
-#   sentimentalFinisher = Finisher() \
-#       .setInputCols([f"{fieldname}_sentimental_classifications"]) \
-#       .setOutputCols([f"{fieldname}_sentimental_token_features"]) \
-#       .setOutputAsArray(True) \
-#       .setCleanAnnotations(False) 
-#   countVectorsSentimental = CountVectorizer(
-#     inputCol=f"{fieldname}_sentimental_token_features", 
-#     outputCol=f"{fieldname}_sentimental_rawFeatures", 
-#     vocabSize=10000, minDF=5)
+  useEmbeddings = UniversalSentenceEncoder.pretrained() \
+    .setInputCols(f"{fieldname}_document") \
+    .setOutputCol(f"{fieldname}_sentence_embeddings")
+  multiClassifierDl = MultiClassifierDLModel.pretrained() \
+      .setInputCols(f"{fieldname}_sentence_embeddings") \
+      .setOutputCol(f"{fieldname}_sentimental_classifications")
+  sentimentalFinisher = Finisher() \
+      .setInputCols([f"{fieldname}_sentimental_classifications"]) \
+      .setOutputCols([f"{fieldname}_sentimental_token_features"]) \
+      .setOutputAsArray(True) \
+      .setCleanAnnotations(False) 
+  countVectorsSentimental = CountVectorizer(
+    inputCol=f"{fieldname}_sentimental_token_features", 
+    outputCol=f"{fieldname}_sentimental_rawFeatures", 
+    vocabSize=10000, minDF=5)
   
   return [
     document_assembler, 
@@ -256,128 +232,84 @@ def NLPPipe(fieldname):
     normalizer,
     finisher,
     countVectors,
-    idf,
     cleaned_token_size,
-#     useEmbeddings,
-#     multiClassifierDl,
-#     sentimentalFinisher,
-#     countVectorsSentimental
-  ]
+    useEmbeddings,
+    multiClassifierDl,
+    sentimentalFinisher,
+    countVectorsSentimental
+  ], [f"{fieldname}_rawFeatures", f"{fieldname}_tokenSize", f"{fieldname}_sentimental_rawFeatures"]
 
 
-# COMMAND ----------
+# More classification docs: https://spark.apache.org/docs/latest/ml-classification-regression.html
 
-# DBTITLE 1,Topic Modeling
-# for interpertation check the bottom cell named TOPIC Lib
-
-
-# Topic modeling
-lda = LDA(k=10, maxIter=10)
-
-# COMMAND ----------
-
-# DBTITLE 1,Control Features
-# control the Features   _idfFeatures
-assembler = VectorAssembler(inputCols=[
-#   "summary_rawFeatures", 
-  "summary_idfFeatures", 
-  "summary_tokenSize", 
-#   "summary_sentimental_rawFeatures",
-#   "reviewText_rawFeatures",  # no idf
-  "reviewText_idfFeatures", 
-  "reviewText_tokenSize", 
-#   "reviewText_sentimental_rawFeatures",
-#   'asin', 
-#   'reviewID',
-#   'reviewTime',
-#   'reviewerName',
-#   'reviewText',
-#   'reviewerID',
-#   'summary',
-  'overall',
-  'verified',
-#   'reviewTime_year',
-#   'reviewTime_month',
-#   'reviewTime_day',
-#   'reviewTime_dayofy',
-#   'reviewTime_week_no',
-#   'reviewerName_Shorthand',
-#   'reviewerName_isAmazon',
-#   'reviewerName_capsName',
-  'reviewTextHasCapsWord',
-  'summaryHasCapsWord',
-#   'reviewTextHasSwearWord',
-#   'summaryHasSwearWord',
-#   'reviewTextNumberExclamation',
-#   'summaryNumberExclamation',
-  'reviewTextNumberComma',
-  'summaryNumberComma',
-  'reviewTextNumberPeriod',
-  'summaryNumberPeriod',
-#   'classWeightCol'  # should be used in LR directly not as feature
-  ], outputCol="features")
-prePipelineStages = NLPPipe("reviewText") + NLPPipe("summary") + [assembler]
-
-# COMMAND ----------
-
-# DBTITLE 1,[Don't touch]Transform
-(trainingData, testingData) = df.randomSplit([0.8, 0.2], seed = 47)
-preprocess = Pipeline(stages=prePipelineStages).fit(trainingData)
-trainingData = preprocess.transform(trainingData)
-testingData = preprocess.transform(testingData)
-
-# COMMAND ----------
-
-# DBTITLE 1,LDA and Regression
-from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
-from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
-
-# other Regression?
-# lr = LogisticRegression(maxIter=20, weightCol="classWeightCol")
-# lr = LogisticRegression(maxIter=20)  # garbage
-gbt = GBTClassifier(maxIter=20)
+# lr = LogisticRegression(maxIter=20, regParam=0.3, elasticNetParam=0, weightCol="classWeightCol")
+lr = LogisticRegression(maxIter=10)  # garbage
 
 paramGrid = ParamGridBuilder()\
-    .addGrid(lr.regParam, [0.1, 0.01, 0.3]) \
+    .addGrid(lr.regParam, [0.5,0.1, 0.01]) \
     .addGrid(lr.fitIntercept, [False, True])\
     .addGrid(lr.elasticNetParam, [0.0, 0.3, 0.5, 1.0])\
     .build()
 
 # In this case the estimator is simply the linear regression.
 # A TrainValidationSplit requires an Estimator, a set of Estimator ParamMaps, and an Evaluator.
-tvs = TrainValidationSplit(estimator=gbt,
+tvs = TrainValidationSplit(estimator=lr,
                            estimatorParamMaps=paramGrid,
-                           evaluator=BinaryClassificationEvaluator(),
-                           # 80% of the data will be used for training, 20% for validation.
-                           trainRatio=0.9, 
-                           seed=42,
-                            parallelism = 1)
+                           evaluator=RegressionEvaluator(),
+                           # 90% of the data will be used for training, 20% for validation.
+                           trainRatio=0.9)
 
+# Topic modeling
+lda = LDA(k=10, maxIter=10)
+
+# Build up the pipeline
+cols = featureList
+pipelineObj = []
+
+for field in [ "reviewText", "summary"]: #, "summary"
+  nlpPipe, output = NLPPipe(field)
+  cols += output
+  pipelineObj += nlpPipe
+  
+assembler = VectorAssembler(inputCols=cols, outputCol="features")
+
+pipelineStages = pipelineObj + [assembler, tvs]
 
 # COMMAND ----------
 
-# DBTITLE 1,[Don't Touch]Transform Training Data
+# DBTITLE 1,Transform Training Data
 # Fit the pipeline to training documents.
-pipelineFit = tvs.fit(trainingData)
+pipeline = Pipeline(stages=pipelineStages)
+pipelineFit = pipeline.fit(trainingData)
 import datetime
 now = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-pipelineFit.save(f"file:///dbfs/joe/{now}_model")
+pipelineFit.save(f"file:///databricks/driver/models/{now}")
 comment = [f"Pipeline name: {now}"]
+comment +=  ["Pipeline object: <insert here>"]
 comment +=  ["Pipeline summary: <insert here>\n\n\n"]
+
+# COMMAND ----------
+
+# pipeline = Pipeline(stages=pipelineObj)
+# eda = pipeline.fit(trainingData).transform(trainingData)
+
+# COMMAND ----------
+
+# eda.selectExpr("summary_sentimental_classifications").show(20, False)
 
 # COMMAND ----------
 
 # DBTITLE 1,Show Training Metrics
 # Extract the summary from the returned LogisticRegressionModel instance trained
 # in the earlier example
-trainingSummary = pipelineFit.summary
+# trainingSummary = pipelineFit.stages[-1].summary
 
-comment += ["Training Accuracy:  " + str(trainingSummary.accuracy)]
-comment += ["Training Precision: " + str(trainingSummary.precisionByLabel)]
-comment += ["Training Recall:    " + str(trainingSummary.recallByLabel)]
-comment += ["Training FMeasure:  " + str(trainingSummary.fMeasureByLabel())]
-comment += ["Training AUC:       " + str(trainingSummary.areaUnderROC)]
-comment += ["\n"]
+# comment += ["Training Accuracy:  " + str(trainingSummary.accuracy)]
+# comment += ["Training Precision: " + str(trainingSummary.precisionByLabel)]
+# comment += ["Training Recall:    " + str(trainingSummary.recallByLabel)]
+# comment += ["Training FMeasure:  " + str(trainingSummary.fMeasureByLabel())]
+# comment += ["Training AUC:       " + str(trainingSummary.areaUnderROC)]
+# comment += ["\n"]
 
 # move to next cell to check
 
@@ -396,7 +328,7 @@ predictions = pipelineFit.transform(testingData)
 # COMMAND ----------
 
 # DBTITLE 1,Use Model to Predict Test Data; Evaluate
-
+from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
 
 acc_evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
 pre_evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="weightedPrecision")
@@ -420,8 +352,7 @@ print((test_df.count(), len(test_df.columns)))
 # COMMAND ----------
 
 test_df, _ = featureEngineering(test_df)
-submit_tranformed = preprocess.transform(test_df)
-submit_predictions = pipelineFit.transform(submit_tranformed)
+submit_predictions = pipelineFit.transform(test_df)
 
 # COMMAND ----------
 
@@ -429,11 +360,13 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import FloatType
 
 lastElement=udf(lambda v:float(v[1]),FloatType())
-submission = submit_predictions.select('reviewID', lastElement('probability').alias("label"))
+submit_predictions.select('reviewID', lastElement('probability').alias("label")).display()
 
 # COMMAND ----------
 
-submission.write.csv(f"file:///dbfs/joe/{now}_sub.csv", header=True)
+!ls models
+!mkdir answers
+!ls
 
 # COMMAND ----------
 
@@ -441,29 +374,3 @@ comment += ["-------------------------------------------------------------------
 # generate a template to put it on top
 for line in comment:
   print(line)
-
-# COMMAND ----------
-
-# DBTITLE 1,LDA Lib
-# # Fit the pipeline to training documents.
-# pipeline = Pipeline(stages=pipelineStages)
-# pipelineFit = pipeline.fit(trainingData)
-# PipelineTransformed = pipelineFit.transform(trainingData)
-
-# # Topic modeling
-# lda = LDA(k=10, maxIter=10)
-# ldaModel = lda.fit(PipelineTransformed)
-
-# transformed = ldaModel.transform(PipelineTransformed).select("topicDistribution")
-# transformed.show(truncate=False)  
-
-# ll = ldaModel.logLikelihood(PipelineTransformed)  
-# lp = ldaModel.logPerplexity(PipelineTransformed) 
-
-# topicIndices = ldaModel.describeTopics(maxTermsPerTopic = wordNumbers)  
-# vocab_broadcast = sc.broadcast(vocabArray)  
-# udf_to_word = udf(to_word, ArrayType(StringType()))  
-  
-# topics = topicIndices.withColumn("words", udf_to_word(topicIndices.termIndices))  
-# topics.show(truncate=False)  
-# exit()
